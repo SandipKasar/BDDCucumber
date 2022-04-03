45e5f5f67b32f1ed2b9585f4f4f2a9d0cf7302c7
Ganesh <gkasar39@gmail.com>
2022-04-03 10:27:18 +0530

    hello

--- AppiumCucumberFramework/src/main/java/com/qa/utils/Demo_1.java
+++ AppiumCucumberFramework/src/main/java/com/qa/utils/Demo_1.java
@@ -3,5 +3,6 @@ package com.qa.utils;
 public class Demo_1 {
 public static void main(String[] args) {
 	System.out.println("New class demo_1 created");
+	System.out.println("new master branch change done");
 }
 }

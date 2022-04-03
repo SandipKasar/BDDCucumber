146a28a7c21e4283ab6e09ea1507ced4128e30f6
Ganesh <gkasar39@gmail.com>
2022-04-03 10:59:49 +0530

    second ganesh change

--- AppiumCucumberFramework/src/main/java/com/qa/utils/Demo_1.java
+++ AppiumCucumberFramework/src/main/java/com/qa/utils/Demo_1.java
@@ -3,11 +3,11 @@ package com.qa.utils;
 public class Demo_1 {
 public static void main(String[] args) {
 	System.out.println("New class demo_1 created");
-<<<<<<< HEAD
+
 	System.out.println("new master branch change done");
-=======
+
 	
 	System.out.println("local branch");
->>>>>>> branch 'master' of https://github.com/SandipKasar/BDDCucumber.git
+
 }
 }
